acfc7849c7890e609374b1658c037d25537cefd3
xidoWeb <erivery2@gmail.com>
2017-11-17 15:54:52 +0900

    float test

---
+++ asset/step_02_web/a_01_basic/js/src/js_basic_07.js
@@ -0,0 +1,11 @@
+// js_basic_07.js
+//var hello = 'hello js';
+
+//console.log(hello);
+
+var day = 0;
+for(; day < 1000; day++){
+  if(day%7 == 0){
+     console.log('day is:'+ day, '고양이에게 캔주기');
+     }
+}
