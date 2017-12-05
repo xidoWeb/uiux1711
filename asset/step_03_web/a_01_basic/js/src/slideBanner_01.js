4c8f2d86ab184bea2824003d4975601617fffaaf
erivery2@gmail.com <erivery2@gmail.com>
2017-12-05 18:37:45 +0900

    slidebanner

--- asset/step_03_web/a_01_basic/js/src/slideBanner_01.js
+++ asset/step_03_web/a_01_basic/js/src/slideBanner_01.js
@@ -3,10 +3,36 @@
 // 즉시 실행 함수(IIFE)
 (function($){
 // jQuery
+var banner = $('.banner');
+var bannerUl = banner.children('ul');
+var bannerLast = bannerUl.children('li');
 
-  $('.banner>ul>li').last().prependTo('.banner>ul');
-  $('.banner>ul').css({marginLeft:'-100%'});
-  $('.banner').css({overflow:'hidden'});
+bannerLast.last().prependTo(bannerUl);
+bannerUl.css({marginLeft:'-100%'});
+banner.css({overflow:'hidden'});
+
+var leftBtn = $('.btn').children('button').first();
+var rightBtn = $('.btn').children('button').last();
+
+leftBtn.on('click', function(event){
+  event.preventDefault(); // ??
+
+  bannerUl.stop(true, false).animate({marginLeft:0}, function(){
+    bannerLast = bannerUl.children('li').last();
+    bannerLast.prependTo(bannerUl);
+    bannerUl.css({marginLeft:'-100%'});
+  });
+}); // leftBtn 클릭
+
+rightBtn.on('click', function(event){
+  event.preventDefault(); // ??
+
+  bannerUl.stop(true, false).animate({marginLeft:'-200%'}, function(){
+    bannerLast = bannerUl.children('li').first();
+    bannerLast.appendTo(bannerUl);
+    bannerUl.css({marginLeft:'-100%'});
+  });
+}); // rightBtn 클릭
 
 })(this.jQuery);
 
