edbaca3ba0447a39241dce493677e82a40bf4955
erivery2@gmail.com <erivery2@gmail.com>
2017-12-07 11:17:05 +0900

    image

--- asset/step_03_web/b_02_jquery_01/js/src/jquery_base_method__02.js
+++ asset/step_03_web/b_02_jquery_01/js/src/jquery_base_method__02.js
@@ -1,6 +1,16 @@
 //  jquery_base_method__02.js
 (function($){
 // jQuery Start
+  var gnbBtn = $('.gnb_btn');
+  var gnbUl = $('#gnb').children('ul');
+
+  gnbBtn.on('click', function(evt){
+    evt.preventDefault();
+    // gnbUl.toggle();
+    gnbUl.slideToggle();
+    // gnbUl.fadeToggle();
+  });
+  // ----------------------------------------
 
 
 
