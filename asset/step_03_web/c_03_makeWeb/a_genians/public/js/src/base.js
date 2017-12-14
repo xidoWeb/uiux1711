4082e493824c43112a34567a6950c439dc880ff1
erivery2@gmail.com <erivery2@gmail.com>
2017-12-14 10:39:15 +0900

    modify

--- asset/step_03_web/c_03_makeWeb/a_genians/public/js/src/base.js
+++ asset/step_03_web/c_03_makeWeb/a_genians/public/js/src/base.js
@@ -2,17 +2,45 @@
   // jquery start ====================================
 
   var wrap = $('#wrap');
-  wrap.prepend('<div id="headBoxWrap"></div>');
-  var headBoxWrap = $('#headBoxWrap');
+ 
+ // 불러올 파일 목록 만들기
 
-  headBoxWrap.load('../../html/src/headBox.html', function() {
+ var baseUrl  = '../../html/src/';
+ var loadFile = {headBox:baseUrl + 'headBox.html',
+                 bannerBox:baseUrl + 'bannerBox.html',
+                 footBox:baseUrl + 'footBox.html'};
+
+  // headBox import
+wrap.prepend('<div id="headBoxWrap"></div>');
+  var headBoxWrap = $('#headBoxWrap');
+  headBoxWrap.load(loadFile.headBox, function() {
     var headBox = $('#headBox');
     var headBoxBg = headBox.css('backgroundColor');
     console.log( headBox );
     headBoxWrap.css({'backgroundColor':headBoxBg});
   });
 
+// bannerBox import
+headBoxWrap.after('<div id="bannerBoxWrap"></div>');
+var bannerBoxWrap = $('#bannerBoxWrap');
+bannerBoxWrap.load(loadFile.bannerBox);
 
+// aboutBox
+// newsBox
+// blogBox
+// lnbBox
+// footNavBox
+
+// footbox import
+    wrap.append('<div id="footBoxWrap"></div>');
+var footBoxWrap = $('#footBoxWrap');
+
+  footBoxWrap.load(loadFile.footBox, function() {
+    var footBox = $('#footBox');
+    var footBoxBg = footBox.css('backgroundColor');
+    console.log( footBox );
+    footBoxWrap.css({'backgroundColor':footBoxBg});
+  });
 
   // jquery end ====================================
 })(this.jQuery);
