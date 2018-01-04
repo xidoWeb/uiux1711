a7419035222bb15426c79dd5b9e1490a85561441
xidoWeb <erivery2@gmail.com>
2018-01-04 09:58:54 +0900

    video sample

--- asset/step_03_web/a_01_basic/js/src/multimedia_03.js
+++ asset/step_03_web/a_01_basic/js/src/multimedia_03.js
@@ -1,14 +1,26 @@
 // multimedia_03.js
 
 (function($) {
+// 경로 및 목록만들기
+  var baseURL = "../multi/audio/";
+  var musicList = [
+                   {title:'veil'},
+                   {title:'CheerU'},
+                   {title:'DoItAgain'},
+                   {title:'Touchdown'},
+                  ];
+  var musicListLength = musicList.length;
 
+// 오디오태그에 기본 내용 삽입하기
   var audio = $('audio');
-
   audio.append('<source>');
   
   var audioSrc = audio.find('source');
-  audioSrc.attr({'src':'../multi/audio/veil.mp3','type':'audio/mp3'});
+  var i = 0;
+  // baseURL + musicList[i].title + '.mp3' // 기본내용삽입
+  audioSrc.attr({'src': baseURL + musicList[i].title + '.mp3','type':'audio/mp3'});
 
+// play 버튼 클릭시, 오디오 재생하기
   var play = $('.play');
   play.on('click', function(e){
     e.preventDefault();
@@ -19,7 +31,16 @@
   var next = $('.next');
   next.on('click', function(e) {
     e.preventDefault();
-    audioSrc.attr({'src':'../multi/audio/Touchdown.mp3','type':'audio/mp3'}); 
+
+    i++; 
+    if(i < musicListLength){
+      audioSrc.attr({'src':baseURL + musicList[i].title + '.mp3',
+                     'type':'audio/mp3'}); 
+    }else{
+      i = 0;
+      audioSrc.attr({'src':baseURL + musicList[i].title + '.mp3',
+                   'type':'audio/mp3'}); 
+    }
     audio[0].load();
     audio[0].play();
   });
@@ -34,7 +55,15 @@
   var prev = $('.prev');
   prev.on('click',function(e) {
     e.preventDefault();
-    audioSrc.attr({'src':'../multi/audio/DoItAgain.mp3','type':'audio/mp3'}); 
+    i--; // 0 -1
+    if(i < 0){
+      i = musicListLength - 1; //3
+      audioSrc.attr({'src':baseURL + musicList[i].title + '.mp3',
+                   'type':'audio/mp3'});
+    }else{
+      audioSrc.attr({'src':baseURL + musicList[i].title + '.mp3',
+                   'type':'audio/mp3'});
+    }
     audio[0].load();
     audio[0].play();
   });
