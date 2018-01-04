34af59f9a9d5adfbcf53a0c7ef5f2015c62986ec
xidoWeb <erivery2@gmail.com>
2018-01-04 12:44:58 +0900

    player fin

--- asset/step_03_web/a_01_basic/js/src/multimedia_04.js
+++ asset/step_03_web/a_01_basic/js/src/multimedia_04.js
@@ -4,11 +4,11 @@
   // 위치값 , 목록, 음악 갯수
   var baseURL = "../multi/video/";
   var videoList = [
-      {title:'asIfItYourLast_BLACKPINK' , singer:'BLACKPINK'},
-      {title:'heartShaker_TWICE'        , singer:'TWICE'},
-      {title:'parade_YOUNHA'            , singer:'YOUNHA'},
-      {title:'peekABoo_RedVelvet'       , singer:'RedVelvet'},
-      {title:'playingWithFire_BLACKPINK', singer:'BLACKPINK'},
+      {title:'asIfItYourLast_BLACKPINK' , singTit:'마지막 처럼'   , singer:'BLACKPINK'},
+      {title:'heartShaker_TWICE'        , singTit:'heartShaker'   , singer:'TWICE'},
+      {title:'parade_YOUNHA'            , singTit:'parade'        , singer:'YOUNHA'},
+      {title:'peekABoo_RedVelvet'       , singTit:'피카부'        , singer:'RedVelvet'},
+      {title:'playingWithFire_BLACKPINK', singTit:'불장난'        , singer:'BLACKPINK'},
                   ];
   var videoLength = videoList.length;
 // --------------------------------------------------------------
@@ -56,7 +56,30 @@ forward.on('click', function(e){
   video[0].load();
   video[0].play();
 });
+// -------------------------------------------------------
+//  목록만들기
+var content = $('.contentList');
+content.append('<ul></ul>');
+var contentBox = content.find('ul');
+for(var j = 0; j < videoLength; j++){
+  contentBox.append('<li><button type="button"><strong>제목: </strong>'+ 
+                      videoList[j].singTit + ' ' + 
+                      '<strong>가수명: </strong>'+
+                      videoList[j].singer + 
+                      '</button></li>');
+}
+content.css({width:'30%', float:'left'});
+contentBox.css({color:'#09a'});
+contentBox.find('button').css({backgroundColor:'transparent', color:'inherit', padding:'0.1rem'});
+contentBox.find('strong').css({fontWeight:'bold'});
 
-
+// li 클릭하여 순서체크기하기
+contentBox.children('li').on('click',function(e) {
+  e.preventDefault();
+  i = $(this).index();
+  videoSource.attr({'src':baseURL + videoList[i].title + '.mp4'});
+  video[0].load();
+  video[0].play();
+});
 
 })(this.jQuery);
