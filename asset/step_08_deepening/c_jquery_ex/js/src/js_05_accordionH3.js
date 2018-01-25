314b275163bd391e69fae4a5ad1a4266bc216402
xidoWeb <erivery2@gmail.com>
2018-01-25 11:44:43 +0900

    accordion3

--- asset/step_08_deepening/c_jquery_ex/js/src/js_05_accordionH3.js
+++ asset/step_08_deepening/c_jquery_ex/js/src/js_05_accordionH3.js
@@ -1,4 +1,14 @@
-// js_05_accordionH1.js
+// js_05_accordionH3.js
 (function($) {
-  
+  var li = $('li').find('a');
+  li.on('focus',function() {
+    $(this).addClass('focus');
+    $(this).parent().css({zIndex:300});
+  });
+
+  li.on('blur', function() {
+    $(this).removeClass('focus');
+    $(this).parent().removeAttr('style');
+  });
+
 })(this.jQuery);
