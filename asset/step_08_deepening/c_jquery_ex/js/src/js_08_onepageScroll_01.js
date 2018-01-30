6a33605fbae2f9a0052bb01814c1ab34908282a9
xidoWeb <erivery2@gmail.com>
2018-01-30 09:14:23 +0900

    modify

--- asset/step_08_deepening/c_jquery_ex/js/src/js_08_onepageScroll_01.js
+++ asset/step_08_deepening/c_jquery_ex/js/src/js_08_onepageScroll_01.js
@@ -1,16 +1,21 @@
 // js_08_onepageScroll_01.js
 
 (function($) {
-  var nav = $('#nav');
-  var navLi = nav.find('li');
-  var timed = 800;
 
-  navLi.on('click', ['a'], function(e) {
+  var indi    = $('.indicator');
+  var indiLi  = indi.find('li');
+  var timed  = 800;
+
+  var navTop = indi.offset().top;
+  // console.log(navTop);
+
+
+  indiLi.on('click', ['a'], function(e) {
     e.preventDefault();
     var _thisAttr = $(this).find('a').attr('href');
     console.log(_thisAttr);
     var offsetTest = $(_thisAttr).offset().top;
     console.log(offsetTest);
-    $('html, body').stop().animate({scrollTop:offsetTest});
+    $('html, body').stop().animate({scrollTop:offsetTest}, timed, 'easeOutBack');
   });
 })(this.jQuery);
