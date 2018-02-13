2f7192f5b26522a7636f55245a4d694a316b742b
xidoWeb <erivery2@gmail.com>
2018-02-13 14:01:47 +0900

    slidebanner_01

--- asset/step_08_deepening/c_jquery_ex/js/src/slide_banner_01.js
+++ asset/step_08_deepening/c_jquery_ex/js/src/slide_banner_01.js
@@ -1,4 +1,52 @@
 // slide_banner_01.js
 (function($) {
+  // banner 첫번째 항목에대해...
+  var banner1 = $('#bannerBox');
+  var oneWrap = banner1.find('.banner_wrap');
+  var oneList = oneWrap.children('li');
+  var oneBtn  = banner1.find('button');
+  var lbtn    = $('.lbtn');
+  var rbtn    = $('.rbtn');
+
+  var oneHeight = oneWrap.height();
+
+  //* 1. banner1 내부의 링크기능을 해제(tabindex="-1");
+
+  // oneList.children('a').attr('tabindex', '-1');
+  // oneList.eq(0).children('a').removeAttr('tabindex');
   
+  $.each(oneList,function(index, value) {  $(this).children('a').attr('tabindex', '-1');  });
+  oneList.eq(0).children('a').removeAttr('tabindex');
+
+  var bannerMv = 0;
+   lbtn.hide();
+  oneBtn.on('click',function(e) {
+    e.preventDefault();
+    var _this     = $(this);
+    var oneMargin = parseInt(oneWrap.css('marginTop'));
+    var oneCut    = oneHeight / 5;
+
+    oneBtn.hide();
+
+    if(_this[0] == lbtn[0] &&  oneMargin < 0 ){
+      (-oneMargin < 0) ? bannerMv = 0  : bannerMv += oneCut; 
+    }else if(_this[0] == rbtn[0]){
+      (-oneMargin > oneCut*3) ? bannerMv = -oneCut*4 : bannerMv -= oneCut; 
+    }
+    // console.log(-bannerMv, oneCut*4);
+    oneWrap.stop().animate({marginTop:bannerMv},function() {
+      // 버튼이 모두 나타나게만들기
+      // oneBtn.show();
+      // 한쪽에서 버튼이 사라지게 만들기
+      if(-bannerMv <= 0){
+        lbtn.hide(); rbtn.show();
+      }else if(-bannerMv >= oneCut*4){
+        rbtn.hide(); lbtn.show(); 
+      }else{
+        lbtn.show(); rbtn.show();
+      }
+    });
+  });
+
+
 })(this.jQuery);
