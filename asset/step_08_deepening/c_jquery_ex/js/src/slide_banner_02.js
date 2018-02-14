2a38db37c8b47843ce84cf3b1c3e0d7adaf1a260
xidoWeb <erivery2@gmail.com>
2018-02-14 12:57:00 +0900

    slidebanner_02-2

--- asset/step_08_deepening/c_jquery_ex/js/src/slide_banner_02.js
+++ asset/step_08_deepening/c_jquery_ex/js/src/slide_banner_02.js
@@ -8,11 +8,38 @@
   // 4. 자동슬라이드기능( setInterval(), clearInterval() )
 // ----------------------------------------------------------
 
+  var bannerBox  =  $('#bannerBox');
+// banner이미지 영역 
+  var bannerWrap =  bannerBox.find('.banner_wrap');
+  var bannerLi   =  bannerWrap.find('li');
+// 인디케이트 영역
+  var indi       = bannerBox.find('.indicator');
+  var indiLi     = indi.find('li');
 
+// - tabindex 제거(처음건 사용)
+  bannerLi.find('a').attr('tabindex', '-1');
+  bannerLi.find('a').eq(0).attr('tabindex', '0');
 
-
-// - tabindex 제거
 // - 1. 인디케이터 클릭시 fade효과
+// .첫인디케이트, (.focus)
+  indiLi.eq(0).addClass('focus');
+
+  indiLi.on('click', ['a'], function(e) {
+    e.preventDefault();
+    var _this = $(this);
+    var _thisI = _this.index();
+
+    indiLi.removeClass('focus');
+    indiLi.eq(_thisI).addClass('focus');
+
+    bannerLi.eq(_thisI).prevAll().fadeOut();
+    bannerLi.eq(_thisI).fadeIn();
+    bannerLi.eq(_thisI).nextAll().fadeIn();
+
+    bannerLi.find('a').attr('tabindex', '-1');
+    bannerLi.find('a').eq(_thisI).attr('tabindex', '0');
+  });
+
 // - 선택의 index값을 가져와서 해당 배너의 링크를 tabindex를 0으로 변경
 
 // - 2. 좌,우 버튼을 클릭시 배너의 내용이 나타나게 만들기
