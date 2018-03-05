7ed2ef52495d0a388f502d540c19a5f87cea6ffb
xidoWeb <erivery2@gmail.com>
2018-03-05 14:57:25 +0900

    modify

--- asset/step_08_deepening/c_jquery_ex/js/src/mousewheel_01.js
+++ asset/step_08_deepening/c_jquery_ex/js/src/mousewheel_01.js
@@ -1,6 +1,8 @@
 // mousewheel_01.js
 (function($) {
   // mousewheel, DOMMouseScroll
+  var headBox = $('#headBox');
+
   var scrollEvent = true;
   $(window).on('mousewheel DOMMouseScroll',function(e) {
     // console.log(e);
@@ -9,24 +11,21 @@
     // console.log(E);
     // console.log(E.detail);  // firefox에만 존재(3의 수치)
     // console.log(E.wheelDelta); // other에만 존재(120의수치)
-
-    
+  
     if(scrollEvent){
+      // ----------------------------------------------
       scrollEvent = false;
-      if(E.detail){
-        // delta = 1; 
-        delta = E.detail * 40; 
-      }else{
-        // delta = -1;
-        delta = -E.wheelDelta;
+      // (E.detail) ? delta = E.detail * 40 : delta = -E.wheelDelta; 
+      (E.detail) ? delta = 1 : delta = -1; 
+      console.log(delta);
+
+      if(delta > 0){ 
+        headBox.stop().animate({top:-100},300, function() { scrollEvent = true; }); 
+      }else if(delta < 0){ 
+        headBox.stop().animate({top:0},300, function() { scrollEvent = true; }); 
       }
-      
+     // ---------------------------------------------  
+    }// if(scrollEvent)
 
-
-      
-      // scrollEvent = false;
-    }
-
-    console.log(delta);
   });
 })(this.jQuery);
