a383c9496f20931b5101cb9a13012226903e7222
xidoWeb <erivery2@gmail.com>
2018-03-07 10:19:23 +0900

    loading

--- asset/step_08_deepening/c_jquery_ex/js/src/loadingPlugin.js
+++ asset/step_08_deepening/c_jquery_ex/js/src/loadingPlugin.js
@@ -1,9 +1,10 @@
 // loadingPlugin.js
 $.fn.loading = function(option) {
+
   var set = $.extend({timed:800},option);
-  setTimeout(function() { //실제 배포시 삭제할 라인
-    return this.fadeOut(set.timed/4, function() {
-      $(this).removeAttr('style').removeClass('loading');
-    });
-  },set.timed*3); //실제 배포시 삭제할 라인
-}
+  
+  return this.fadeOut(set.timed, function() {
+    $(this).removeAttr('style').removeClass('loading');
+  });
+
+};
