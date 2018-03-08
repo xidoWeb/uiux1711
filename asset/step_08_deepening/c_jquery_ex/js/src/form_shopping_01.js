e35f671c9c8103e7b8d5038beedc8ee9f9331efd
xidoWeb <erivery2@gmail.com>
2018-03-08 14:09:57 +0900

    shop step3'

--- asset/step_08_deepening/c_jquery_ex/js/src/form_shopping_01.js
+++ asset/step_08_deepening/c_jquery_ex/js/src/form_shopping_01.js
@@ -9,37 +9,75 @@
     4. 택배요금 포함여부 확인하여 금액 가져오기
     5. 1~5내용을 합산하여 총금액에 결과 도출하기
   */
+
+  var num = 1;
+  var more = 0;
+  var send = 0;
+  
   // 1. 기본금액 만들기
+
   var  basePrice = $('dd.price').children('span').text();
   var  bp = parseInt( basePrice.replace(',','') );
   // console.log( bp );
 
+
+  // 결과 체크
+  var result = $('.result').find('span');
+  var resultFn = function(num,more) {
+    var count = num || 1, more = more;
+    var resultPrice = bp*count+more;
+    var totalR = resultPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    result.text(totalR);
+    // console.log(num,more);
+  };
+  resultFn(num,more);
+
    // 2. 수량 입력(+,-) 기능과 해당금액 가져오기
    var amountBtn = $('dd.select').find('button');
    var countInput = $('#num');
-   var num = 1;
+   //console.log(amountBtn);
 
-   // console.log(amountBtn);
-   // $.each(amountBtn,function(i,v) {
-   //  $(this).on('click',function() {
-   //    // console.log(i)
-   //    (i == 0) ? console.log('minus'): console.log('plus');
-   //  });
-   // });
-
-   amountBtn.on('click',function(e) {
+// 값 버튼으로 클릭
+  /* 
+    // 방법1: each문으로 사용
+    $.each(amountBtn,function(i,v) {
+     $(this).on('click',function() {
+       // console.log(i)
+       (i == 0) ? console.log('minus'): console.log('plus');
+     });
+    });
+    */
+  //방법2 html문서에 class값 입력하여 사용
+  amountBtn.on('click',function(e) {
     e.preventDefault();
     var btnI = $(this)[0];
     // console.log(btnI);
-    if(btnI == $('.minus')[0]){
-      (num <= 1) ? num = 1 : num--;
-    }else{
-      (num >= 10) ? num = 10 : num++;
-    }
+    if(btnI == $('.minus')[0]){ (num <= 1) ? num = 1 : num--;  }else{  (num >= 10) ? num = 10 : num++;  }
     countInput.val(num);
-    // console.log(num);
-   });
+    resultFn(num,more);
+  });
+
+// 수량 직접입력
+  countInput.on('change',function() {
+    num = parseInt($(this).val());
+    resultFn(num,more);
+  });
+
+// 옵션 선택
+  var optionProduct = $('#optionProduct');
+  optionProduct.on('change',function(e) {
+    more = $(this).children('option:selected').data('price');
+    if( more==undefined ){ more=0;}
+     // console.log(more);
+    resultFn(num,more);
+  });
 
 
+  // 택배배송 여부 
+  // var sendQue = $('dd.send').find('input');
+  // sendQue.on('change', function(e) {
+  //   e.preventDefault();
+  //   $(this).prop('checked',true);
+  // });
   // ---------------------------------------------
 })(this.jQuery);
